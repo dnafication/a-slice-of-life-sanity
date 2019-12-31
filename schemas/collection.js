2bb918a63a5f09a7d4c2bbdb655afde24e134d24
dnafication <dnafication@users.noreply.github.com>
2019-12-31 21:10:26 +1100

    Update collection.js

--- schemas/collection.js
+++ schemas/collection.js
@@ -43,8 +43,8 @@ export default {
       type: 'datetime'
     },
     {
-      name: 'body',
-      title: 'Body',
+      name: 'description',
+      title: 'Description',
       type: 'blockContent'
     }
   ],
